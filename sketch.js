0ca4fe79d3ed4c43f6a611715fe57abfab48b167
gksktnsdl <gksktnsdl@naver.com>
2014-10-02 16:49:51 +0900

    0

--- sketch.js
+++ sketch.js
@@ -2,13 +2,13 @@ function setup()
  {
   createCanvas(800, 600);
   
-  stroke(255);
+  stroke(255,255,255,50);
 }
 
   function draw()
 {
   background(174,138,184);
-  fill(255,50);
+  fill(255,255,255,50);
  if (mouseX < 100) {
   rect(50, 100, 100,100);} else if (mouseX < 200){
   rect(150, 200,200,200);} else if (mouseX < 300){
