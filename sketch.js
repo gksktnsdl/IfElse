b4fdd233efa7187e817732e52bf0fb75143a2c0e
gksktnsdl <gksktnsdl@naver.com>
2014-10-02 16:45:06 +0900

    0

--- sketch.js
+++ sketch.js
@@ -9,16 +9,13 @@ function setup()
 {
   background(174,138,184);
  if (mouseX < 100) {
-  ellipse(50, 200, 100, 100);
-} else if (mouseX < 200){
-  ellipse(150, 200, 100, 100);
-} else if (mouseX < 300){
-  ellipse(250, 200, 100, 100);
-} else if (mouseX < 400){
-  ellipse(350, 200, 100, 100);
-} else if (mouseX < 500){
-  ellipse(450, 200, 100, 100);
-} else {
-  ellipse(550, 200, 100, 100);
-}
+  rect(50, 100, 100,100);} else if (mouseX < 200){
+  rect(150, 200,200,200);} else if (mouseX < 300){
+ rect(250, 100, 200,200);} else if (mouseX < 400){
+  rect(350,100, 100,200);} else if (mouseX < 500){
+  rect(450,100, 200,100);} else {
+  rect(500,500,100,100);}
+
+
+
 }
